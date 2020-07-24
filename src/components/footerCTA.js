e83dc540d0366f98318e9d2c5303af08c19bdb1e
Cecelia Martinez <60482585+CypressCecelia@users.noreply.github.com>
2020-07-24 13:52:11 -0400

    Update cypress project (#10)
    * Bump websocket-extensions from 0.1.3 to 0.1.4
    
    Bumps [websocket-extensions](https://github.com/faye/websocket-extensions-node) from 0.1.3 to 0.1.4.
    - [Release notes](https://github.com/faye/websocket-extensions-node/releases)
    - [Changelog](https://github.com/faye/websocket-extensions-node/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/faye/websocket-extensions-node/compare/0.1.3...0.1.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * updated main image and changed image component to be specific to avatar
    
    * added border to images and bumped up font size
    
    * adds header background, styling to text, adds emojis to menu links and overrides activeStyle
    
    * updating speaking page and created additional components.
    
    * fixes link to avatarimage
    
    * changes header background
    
    * updates cypress version and corrects project ID for recording to Dashboard
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Cecelia Martinez <40367173+ceceliacreates@users.noreply.github.com>

---
+++ src/components/footerCTA.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const FooterCTA = () => (
+  <div style={{ textAlign: "center" }}>
+    <p>
+      Want to team up for an event? Message me on{" "}
+      <a
+        href="https://www.twitter.com/ceceliacreates"
+        target="blank"
+        style={{ textDecoration: "none", color: "#60BBCD" }}
+      >
+        Twitter
+      </a>
+      ,{" "}
+      <a
+        href="https://www.github.com/ceceliacreates"
+        target="blank"
+        style={{ textDecoration: "none", color: "#60BBCD" }}
+      >
+        GitHub
+      </a>
+      , and{" "}
+      <a
+        href="https://www.twitch.tv/ceceliacreates"
+        target="blank"
+        style={{ textDecoration: "none", color: "#60BBCD" }}
+      >
+        Twitch
+      </a>
+    </p>
+  </div>
+)
+
+export default FooterCTA
--- src/components/header.js
+++ src/components/header.js
@@ -1,70 +1,68 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle, siteDescription }) => (
-  <header
-    style={{
-      background: `#60BBCD`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        textAlign: 'center'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <h3
-        style={{
-          fontStyle: 'italic'
+  <header>
+    <Link id="site-title" to="/">
+      {siteTitle}
+    </Link>
+    <h3 id="site-description">{siteDescription}</h3>
+    <h5>
+      <Link
+        activeStyle={{
+          margin: "0.25em",
+          fontSize: "1.5em",
         }}
-      >{siteDescription}</h3>
-    </div>
-    <div 
-    style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem .5rem`,
-        textAlign: 'center'
-      }}>
-    <h5 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >Home</Link> ||  
-         <Link
-          to="/faq"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        > FAQ</Link>
-        ||  
-         <Link
-          to="/speaking"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        > Speaking</Link>
-      </h5>
-    </div>
+        class="menu-link"
+        to="/"
+      >
+        <span role="img" aria-label="home">
+          🏡
+        </span>{" "}
+        Home
+      </Link>{" "}
+      <Link
+        activeStyle={{
+          margin: "0.25em",
+          fontSize: "1.5em",
+        }}
+        class="menu-link"
+        to="/faq"
+      >
+        <span role="img" aria-label="woman-raising-one-hand">
+          🙋🏽‍♀️
+        </span>{" "}
+        FAQ
+      </Link>{" "}
+      <Link
+        activeStyle={{
+          margin: "0.25em",
+          fontSize: "1.5em",
+        }}
+        class="menu-link"
+        to="/speaking"
+      >
+        <span role="img" aria-label="cheering-megaphone">
+          📣
+        </span>{" "}
+        Speaking
+      </Link>
+      {/* <Link
+        activeStyle={{
+          margin: "0.25em",
+          fontSize: "1.5em",
+        }}
+        class="menu-link"
+        to="/resources"
+      >
+        <span role="img" aria-label="books">
+          📚
+        </span>{" "}
+        Resources
+      </Link>{" "} */}
+    </h5>
   </header>
 )
 
